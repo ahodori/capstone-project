9c9c270e0e8c97920ad810bf8748c349d58da15a
ahodori <samuelbhendricks@gmail.com>
2022-07-05 12:38:22 -0400

    added react router and material ui dependencies, basic routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
 
 function App() {
+  [currentUser, setCurrentUser] = useState({});
+  [isLoggedIn, setIsLoggedIn]   = useState(false);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <div className="App">
+          <Header/>
+          {/* <Routes>
+            <Route path="/" element={<Homepage/>}/>
+            <Route path="user">
+              <Route path=":id" element={<UserProfile/>}/>
+            </Route>
+            <Route path="wikiblog">
+              <Route path=":id">
+                <Route path="/" element={<Wikiblog/>}/>
+                <Route path="edit" element={<EditWikiblog/>}/>
+              </Route>
+            </Route>
+          </Routes> */}
+        </div>
+    </BrowserRouter>
   );
 }
 
