0324cf2c09996179ad5f512c22aac6472f007b99
ahodori <samuelbhendricks@gmail.com>
2022-07-07 10:46:25 -0400

    added additional fields to versioned pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,10 @@
 class Page < ApplicationRecord
     belongs_to :wikiblog
     has_many :page_versions
+
+    def text
+        current_version = page_versions.where(is_current_version: true)
+        current_version.text if current_version
+        ""
+    end
 end
