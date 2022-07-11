941d69fe3a2d6c33f15d6ece43907e583314bf37
ahodori <samuelbhendricks@gmail.com>
2022-07-11 11:36:13 -0400

    wikiblogs get all route displays appropriate info

--- app/serializers/wikiblog_serializer.rb
+++ app/serializers/wikiblog_serializer.rb
@@ -1,3 +1,16 @@
 class WikiblogSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :user, :pagenum, :updated
+
+  def user
+    {user_id: object.user.id, username: object.user.username}
+  end
+
+  def pagenum
+    object.pages.length
+  end
+
+  def updated
+    object.pages.find_by(is_index: true).updated_at
+  end
+
 end
