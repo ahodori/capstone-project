e3e1d43fbb23cda9eed24b58db5beac2208cbd77
ahodori <samuelbhendricks@gmail.com>
2022-07-11 10:32:41 -0400

    creating new wikiblog creates editorship association to creator user

--- app/models/wikiblog.rb
+++ app/models/wikiblog.rb
@@ -2,4 +2,19 @@ class Wikiblog < ApplicationRecord
     belongs_to :user
     has_many :pages
     has_many :editorships
+
+    validates :name, presence: true
+    
+    after_create :create_editorship
+
+    private
+
+    def create_editorship
+        e = Editorship.create_or_find_by(user_id: self.user.id, wikiblog_id: self.id)
+        # if e.valid?
+        #     puts "created editorship for wikiblog " + self.id.to_s + " with user " + self.user.id.to_s
+        # else
+        #     puts "COULDN'T make editorship for wikiblog " + self.id.to_s + " with user " + self.user.id.to_s
+        # end
+    end
 end
