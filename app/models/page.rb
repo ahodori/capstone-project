8cdf672e51c1ba8cead755e8db807d8653e883f9
ahodori <samuelbhendricks@gmail.com>
2022-07-11 10:37:38 -0400

    creating new page automatically sets most recent to index

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,8 @@ class Page < ApplicationRecord
     belongs_to :wikiblog
     has_many :page_versions
 
+    after_validation :set_index, on: [:create]
+
     def text
         current_version = page_versions.where(is_current_version: true)
         return current_version.pluck(:text) if current_version
@@ -12,5 +14,17 @@ class Page < ApplicationRecord
         current_version = page_versions.where(is_current_version: true)
         future_versions = page_versions.where("updated_at > ?", current_version.pluck(:updated_at))
         future_versions
+    end
+
+    private
+
+    def set_index
+        past_index = Page.where(wikiblog_id: self.wikiblog.id, is_index: true)
+        if past_index
+            past_index.each do |page|
+                page.update(is_index: false)
+            end
+        end
+        self.is_index = true
     end
 end
