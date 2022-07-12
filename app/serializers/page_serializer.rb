668ee491e5d4fd89cd3d53877e35b6feaafdbd75
ahodori <samuelbhendricks@gmail.com>
2022-07-12 10:05:04 -0400

    page serializer sends updated time of current version

--- app/serializers/page_serializer.rb
+++ app/serializers/page_serializer.rb
@@ -1,6 +1,10 @@
 class PageSerializer < ActiveModel::Serializer
-  attributes :id, :text, :title, :is_index
+  attributes :id, :text, :title, :is_index, :updated
   def text
     object.text[0]
+  end
+
+  def updated
+    object.page_versions.find_by(is_current_version: true).updated_at
   end
 end
