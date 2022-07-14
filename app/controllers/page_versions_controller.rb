ea3233223935fc7e93f80e9c5f8c648caa8f21bd
ahodori <samuelbhendricks@gmail.com>
2022-07-14 18:26:15 -0400

    authorization for pageversions

--- app/controllers/page_versions_controller.rb
+++ app/controllers/page_versions_controller.rb
@@ -1,5 +1,11 @@
 class PageVersionsController < ApplicationController
     def create
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+
+        page = Page.find_by(id: params[:page_id])
+        return render json: { error: "Page not found" }, status: 404 unless page
+        return render json: { error: "Not authorized" }, status: :unauthorized unless page.wikiblog.editorships.find_by("user_id = ?", session[:user_id])
+
         page_version = PageVersion.create(page_version_params)
         if page_version.valid?
             render json: page_version
