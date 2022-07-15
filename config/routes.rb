4b64c697a27c44dc3c8ea5298b2635e1e8a25ba4
ahodori <samuelbhendricks@gmail.com>
2022-07-15 00:04:48 -0400

    added fallback page

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
   get "/me", to: "users#show_session"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
