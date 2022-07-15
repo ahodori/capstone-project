98752bb8434df36d865fc0ff46e78cf1ecf8e1dc
ahodori <samuelbhendricks@gmail.com>
2022-07-15 00:33:12 -0400

    fixed blank page issue on deploy

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,5 +1,9 @@
 class FallbackController < ApplicationController
-    def index
-        redirect_back
-      end
+    include ActionController::MimeResponds
+
+  def index
+    respond_to do |format|
+      format.html { render body: Rails.root.join('public/index.html').read }
+    end
+  end
 end
